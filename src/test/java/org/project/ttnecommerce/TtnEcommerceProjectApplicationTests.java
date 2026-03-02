0db1420716d4962c6960d4b9a74a1dc6be55fdbf
rawatrohit-07 <rohit.rawat1@tothenew.com>
2026-03-02 15:45:19 +0530

    -setting Environment

---
+++ src/test/java/org/project/ttnecommerce/TtnEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.ttnecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtnEcommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
